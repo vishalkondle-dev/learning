93471b4288affb08c25fdf22e731e3cce9a6040e
Vishal Kondle <71583944+vishalkondle-dev@users.noreply.github.com>
2025-03-12 20:03:54 +0530

    Java-OOP-Polymorphism

---
+++ _java/ObjectOrientedProgramming/Polymorphism/MethodOverriding.java
@@ -0,0 +1,27 @@
+package _java.ObjectOrientedProgramming.Polymorphism;
+
+class Language {
+    public void displayInfo() {
+        System.out.println("Common English Language");
+    }
+}
+
+class Java extends Language {
+    @Override
+    public void displayInfo() {
+        System.out.println("Java Programming Language");
+    }
+}
+
+class MethodOverriding {
+    public static void main(String[] args) {
+
+        // create an object of Java class
+        Java j1 = new Java();
+        j1.displayInfo();
+
+        // create an object of Language class
+        Language l1 = new Language();
+        l1.displayInfo();
+    }
+}
