db6fff47a880ff42f88b6c81694024dc01a8ccc7
Vishal Kondle <71583944+vishalkondle-dev@users.noreply.github.com>
2025-03-13 23:49:52 +0530

    Java-Java8-Functional_Interface

---
+++ _java/Java8/FunctionalInterface.java
@@ -0,0 +1,21 @@
+package _java.Java8;
+
+@FunctionalInterface
+interface MyFunctionalInterface {
+
+    // abstract method
+    void abstractMethod();
+    // void abstractMethod1(); // Error: Multiple non-overriding abstract methods
+    // found in interface MyFunctionalInterface
+
+    // default method
+    default void defaultMethod() {
+        System.out.println("Default Method");
+    }
+
+    // static method
+    static void staticMethod() {
+        System.out.println("Static Method");
+    }
+
+}
