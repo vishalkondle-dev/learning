27325b91b42708a0b91a02bb5b775dccb9c147f5
Vishal Kondle <71583944+vishalkondle-dev@users.noreply.github.com>
2025-03-13 23:56:55 +0530

    Java-Java8-Functional_Interface_Inheritance

--- _java/Java8/FunctionalInterfaceInheritance.java
+++ _java/Java8/FunctionalInterfaceInheritance.java
@@ -1,19 +1,18 @@
 package _java.Java8;
 
 @FunctionalInterface
-interface MyFunctionalInterface {
+interface FunctionalInterfaceInheritance extends MyFunctionalInterface {
 
-    // abstract method
+    @Override
     void abstractMethod();
-    // void abstractMethod1(); // Error: Multiple non-overriding abstract methods
-    // found in interface MyFunctionalInterface
 
-    // default method
+    // void newMethod(); // Error: FunctionalInterfaceInheritance is not a
+    // functional interface
+
     default void defaultMethod() {
         System.out.println("Default Method");
     }
 
-    // static method
     static void staticMethod() {
         System.out.println("Static Method");
     }
