bb6751faab617c55ce6df60cf64bdfc8f858336b
Vishal Kondle <71583944+vishalkondle-dev@users.noreply.github.com>
2025-03-16 19:10:14 +0530

    Java-Multithreading-Synchronization

---
+++ _java/Multithreading/Synchronization/Counter.java
@@ -0,0 +1,13 @@
+package _java.Multithreading.Synchronization;
+
+public class Counter {
+    public int count = 0;
+
+    public void increment() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
