f30a81bd8135e11a559b6ac6388c93d8b32dfe6e
Vishal Kondle <71583944+vishalkondle-dev@users.noreply.github.com>
2025-03-18 21:35:21 +0530

    Java+SpringBoot+Creating REST API

---
+++ spring_boot/journalApp/src/main/java/net/engineeringdigest/journalApp/entity/Journal.java
@@ -0,0 +1,30 @@
+package net.engineeringdigest.journalApp.entity;
+
+public class Journal {
+    private long id;
+    private String title, content;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
