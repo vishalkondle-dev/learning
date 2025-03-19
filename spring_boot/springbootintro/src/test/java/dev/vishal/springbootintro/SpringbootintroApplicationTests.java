36a5ca945fa1a9193569020efa26dd7ac351e52b
Vishal Kondle <71583944+vishalkondle-dev@users.noreply.github.com>
2025-03-20 02:23:58 +0530

    SpringBoot-Springboot Initializer

---
+++ spring_boot/springbootintro/src/test/java/dev/vishal/springbootintro/SpringbootintroApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vishal.springbootintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootintroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
