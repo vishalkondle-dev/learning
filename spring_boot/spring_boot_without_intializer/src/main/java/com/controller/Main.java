a728ecfe80f4f5b2c13188fd31fc0ad9d95ed2d6
Vishal Kondle <71583944+vishalkondle-dev@users.noreply.github.com>
2025-03-24 16:05:03 +0530

    SpringBoot-Spring boot application without Spring boot initializer

---
+++ spring_boot/spring_boot_without_intializer/src/main/java/com/controller/Main.java
@@ -0,0 +1,11 @@
+package com.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // it will be having all the configurations
+public class Main {
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class);
+	}
+}
