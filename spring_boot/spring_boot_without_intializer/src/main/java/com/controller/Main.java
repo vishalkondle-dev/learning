67e31b02c787c2365229ede3b1d2eb5ce8749629
Vishal Kondle <71583944+vishalkondle-dev@users.noreply.github.com>
2025-03-24 16:34:09 +0530

    SpringBoot-args passed to run method

--- spring_boot/spring_boot_without_intializer/src/main/java/com/controller/Main.java
+++ spring_boot/spring_boot_without_intializer/src/main/java/com/controller/Main.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication // it will be having all the configurations
 public class Main {
 	public static void main(String[] args) {
-		SpringApplication.run(Main.class);
+		SpringApplication.run(Main.class, args);
 	}
 }
